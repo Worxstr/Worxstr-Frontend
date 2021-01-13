58c6599e9ef4d0bad8d8c132f2c3383f7bc2179c
Jackson Sippe <jackson123198@gmail.com>
2021-01-13 11:34:21 -0500

    Clean up home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="home fill-height d-flex flex-column justify-center align-center">
     <h2 class="text-h2 font-weight-bold mb-2">Worxstr</h2>
-    <p class="text-subtitle-2">Uhhhh idk what else to put here</p>
+    <p class="text-subtitle-2"></p>
 
     <v-btn v-if="authenticatedUser" @click="signOut" color="primary">Sign out</v-btn>
     <v-btn v-else :to="{name: 'signIn'}" color="primary">Sign in</v-btn>
