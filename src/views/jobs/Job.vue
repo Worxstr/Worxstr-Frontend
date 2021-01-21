a15b2e9dfb17c7ccf4a6466e29e05750d97a66c9
Alex Wohlbruck <alexwohlbruck@gmail.com>
2021-01-21 14:59:08 -0500

    Add marker to job map

--- src/views/jobs/Job.vue
+++ src/views/jobs/Job.vue
@@ -12,22 +12,12 @@
       <l-map
         class="align-self-stretch"
         style="height: 400px; z-index: 0"
-        :zoom="15"
-        :center="center"
+        :zoom="18"
+        :center="location"
         :options="mapOptions"
       >
         <l-tile-layer :url="url" :attribution="attribution" />
-        <l-marker :lat-lng="withPopup">
-          <!-- <l-popup>
-            <div @click="innerClick">
-              I am a popup
-              <p v-show="showParagraph">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-                sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
-                Donec finibus semper metus id malesuada.
-              </p>
-            </div>
-          </l-popup> -->
+        <l-marker :lat-lng="location" :icon="markerIcon">
         </l-marker>
       </l-map>
       <v-card-text>
@@ -63,7 +53,10 @@
       <v-btn text @click="addShift">Add new shift</v-btn>
     </v-toolbar>
 
-    <p v-if="!job.shifts || !job.shifts.length" class="text-body-2 text-center mt-3">
+    <p
+      v-if="!job.shifts || !job.shifts.length"
+      class="text-body-2 text-center mt-3"
+    >
       There aren't any shifts for this job.
     </p>
 
@@ -95,7 +88,7 @@
       </v-expansion-panel>
     </v-expansion-panels>
 
-    <edit-job-dialog :opened.sync="editJobDialog" :job.sync="job"/>
+    <edit-job-dialog :opened.sync="editJobDialog" :job.sync="job" />
 
     <edit-shift-dialog
       :opened.sync="editShiftDialog"
@@ -107,9 +100,11 @@
 
 <script>
 /* eslint-disable @typescript-eslint/camelcase */
-import { latLng } from "leaflet";
+import { latLng, divIcon } from "leaflet";
 import EditJobDialog from "./EditJobDialog";
 import EditShiftDialog from "./EditShiftDialog";
+
+// import markerIcon from '@/assets/icons/map-marker.svg'
 
 export default {
   name: "job",
@@ -117,6 +112,9 @@ export default {
   computed: {
     job() {
       return this.$store.getters.job(this.$route.params.jobId);
+    },
+    location() {
+      return latLng(this.job.latitude, this.job.longitude);
     },
   },
   mounted() {
@@ -135,14 +133,18 @@ export default {
       return `${employee.first_name} ${employee.last_name}`;
     },
     organizationManagerName(organizationManagerId) {
-      if (!this.job.managers) return '';
-      const m = this.job.managers.organization_managers.find((m) => m.manager_id == organizationManagerId);
-      return m ? `${m.first_name} ${m.last_name}` : '';
+      if (!this.job.managers) return "";
+      const m = this.job.managers.organization_managers.find(
+        (m) => m.manager_id == organizationManagerId
+      );
+      return m ? `${m.first_name} ${m.last_name}` : "";
     },
     employeeManagerName(employeeManagerId) {
-      if (!this.job.managers) return '';
-      const m = this.job.managers.employee_managers.find((m) => m.id == employeeManagerId);
-      return m ? `${m.first_name} ${m.last_name}` : '';
+      if (!this.job.managers) return "";
+      const m = this.job.managers.employee_managers.find(
+        (m) => m.id == employeeManagerId
+      );
+      return m ? `${m.first_name} ${m.last_name}` : "";
     },
     addShift() {
       return;
@@ -155,6 +157,18 @@ export default {
     shifts: [],
 
     // Leaflet data
+    markerIcon: divIcon({
+      html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 34.892337" height="60" width="40">
+        <g transform="translate(-814.59595,-274.38623)">
+          <g transform="matrix(1.1855854,0,0,1.1855854,-151.17715,-57.3976)">
+            <path d="m 817.11249,282.97118 c -1.25816,1.34277 -2.04623,3.29881 -2.01563,5.13867 0.0639,3.84476 1.79693,5.3002 4.56836,10.59179 0.99832,2.32851 2.04027,4.79237 3.03125,8.87305 0.13772,0.60193 0.27203,1.16104 0.33416,1.20948 0.0621,0.0485 0.19644,-0.51262 0.33416,-1.11455 0.99098,-4.08068 2.03293,-6.54258 3.03125,-8.87109 2.77143,-5.29159 4.50444,-6.74704 4.56836,-10.5918 0.0306,-1.83986 -0.75942,-3.79785 -2.01758,-5.14062 -1.43724,-1.53389 -3.60504,-2.66908 -5.91619,-2.71655 -2.31115,-0.0475 -4.4809,1.08773 -5.91814,2.62162 z" style="fill:${'#d10000'};"/>
+            <circle r="3.0355" cy="288.25278" cx="823.03064" id="path3049" style="display:inline;fill:${'#610000'};"/>
+          </g>
+        </g>
+      </svg>`,
+      iconAnchor: [20, 55],
+      className: 'marker'
+    }),
     center: latLng(36.2141575, -81.6820535),
     url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     attribution:
@@ -171,3 +185,10 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+  .marker {
+    background: none;
+    border: none;
+  }
+</style>
