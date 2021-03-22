82d23c19a16a4a127cab684809f31df7788c703b
Alex Wohlbruck <alexwohlbruck@gmail.com>
2021-03-22 00:30:49 -0400

    Add mailto link to contact form (#68)

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,31 +1,22 @@
 <template lang="pug">
 v-container.d-flex.flex-column.justify-center.align-center.pa-16
-	p.text-h4.font-weight-bold.mb-15 Contact us
+	p.text-h4.font-weight-bold.mb-10 Contact us
 
 	v-form.flex-grow-1.d-flex.flex-column(@submit.prevent="updateTimecard", v-model="isValid" style='width: 600px; max-width: 100%')
 				
 		v-text-field(
 			outlined,
 			dense,
-			label="Your name",
-			v-model="name",
-			:rules="f",
+			label="Subject",
+			v-model="subject",
 			required
 		)
-		v-text-field(
-			outlined,
-			dense,
-			label="Your email",
-			v-model="name",
-			:rules="f",
-			required
-		)
+
 		v-textarea(
 			outlined,
 			dense,
 			label="Message",
-			v-model="name",
-			:rules="f",
+			v-model="message",
 			required
 		)
 
@@ -33,14 +24,24 @@ v-container.d-flex.flex-column.justify-center.align-center.pa-16
 		
 		v-card-actions
 			v-spacer
-			v-btn(text, @click="updateTimecard") Send message
+			v-btn(text, @click='openEmailClient') Send message
 </template>
 
 <script>
 export default {
-	name: "contact",
-	metaInfo: {
-		title: "Contact us",
-	},
+  name: "contact",
+  metaInfo: {
+    title: "Contact us",
+  },
+  data: () => ({
+		subject: "",
+    message: "",
+  }),
+  methods: {
+    openEmailClient() {
+      const win = window.open(`mailto:support@worxstr.com?subject=${this.subject}&body=${this.message}`, "_blank")
+      win.focus()
+    },
+  },
 };
 </script>
