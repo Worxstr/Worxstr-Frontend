8a3443eaa5a8f3f457559a14f3a71e0a81afa07f
Alex Wohlbruck <alexwohlbruck@gmail.com>
2021-06-13 20:36:19 -0400

    Add feedback prompt to calculator (#x7gf9y)

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+v-form.flex-grow-1.d-flex.flex-column(
+  @submit.prevent="updateTimecard",
+  v-model="isValid",
+  style="width: 600px; max-width: 100%"
+)
+	v-text-field(
+		outlined,
+		filled,
+		dense,
+		label="Subject",
+		v-model="subject",
+		required
+	)
+
+	v-textarea(
+		outlined,
+		filled,
+		dense,
+		label="Message",
+		v-model="message",
+		required,
+		hide-details
+	)
+
+	v-spacer
+
+	v-card-actions
+		v-spacer
+		v-btn(text, @click="openEmailClient") Send message
+
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ContactForm extends Vue {
+	
+}
+</script>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,41 +2,19 @@
 v-container.d-flex.flex-column.justify-center.align-center.pt-8
   p.text-h4.font-weight-bold.mb-10 Contact us
 
-  v-form.flex-grow-1.d-flex.flex-column(
-    @submit.prevent="updateTimecard",
-    v-model="isValid",
-    style="width: 600px; max-width: 100%"
-  )
-    v-text-field(
-      outlined,
-      filled,
-      dense,
-      label="Subject",
-      v-model="subject",
-      required
-    )
-
-    v-textarea(
-      outlined,
-      filled,
-      dense,
-      label="Message",
-      v-model="message",
-      required
-    )
-
-    v-spacer
-
-    v-card-actions
-      v-spacer
-      v-btn(text, @click="openEmailClient") Send message
+  contact-form
 </template>
 
 <script>
+import ContactForm from '@/components/ContactForm.vue'
+
 export default {
   name: "contact",
   metaInfo: {
     title: "Contact us",
+  },
+  components: {
+    ContactForm,
   },
   mounted() {
     if (this.$route.params.subject) {
