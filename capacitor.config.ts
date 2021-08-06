8205ca49c146408d45788f5e8e111d6f450e6fa4
Alex Wohlbruck <alexwohlbruck@gmail.com>
2021-08-06 13:07:22 -0400

    Remove service worker

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -13,7 +13,7 @@ const config: CapacitorConfig = {
   cordova: {},
   server: {
     // hostname: "worxstr.com",
-    "url": "http://192.168.86.24:8080",
+    "url": "http://192.168.86.42:8080",
     "cleartext": true
   }
 };
