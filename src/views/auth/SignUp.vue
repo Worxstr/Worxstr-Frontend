695bd9beb583ffe9b9c0a6bd4fd25f709b8f5046
Alex Wohlbruck <alexwohlbruck@gmail.com>
2021-09-27 23:44:27 -0400

    Fix dwolla form cut-off and force light mode on sign up forms

--- src/views/auth/SignUp.vue
+++ src/views/auth/SignUp.vue
@@ -2,7 +2,7 @@
 div
   v-container.sign-in.fill-height.d-flex.flex-column.justify-center.align-center.arrow-container
     
-    v-card.soft-shadow(width='520' style='overflow-y: auto')
+    v-card.soft-shadow(width='520' style='overflow-y: auto' light)
   
       v-form(@submit.prevent='signUp' v-model='isValid')
         v-card-title.text-h5
@@ -10,7 +10,7 @@ div
           span(v-else) Sign up as a {{ accountType == 'org' ? 'business' : 'contractor' }}
 
         v-card-text.pb-0
-          v-window.pt-2(v-model='step' touchless :style='step == 1 && `padding-bottom: ${$vuetify.breakpoint.xs ? "60px" : "20px"}`')
+          v-window.pt-2(v-model='step' touchless :style="step == 1 && 'padding-bottom: 20px'")
 
             v-window-item(:value='0')
               .pa-1.d-flex.flex-column.flex-sm-row.justify-center
@@ -21,7 +21,7 @@ div
                   v-icon mdi-domain
                   span.ml-3.text-h6 I have a business
 
-            v-window-item(:value='1' :style='$vuetify.breakpoint.xs && `height: calc(80vh - 70px)`')
+            v-window-item(:value='1' :style='$vuetify.breakpoint.xs && `min-height: calc(80vh - 70px)`')
               p(v-if="accountType == 'org'")
                 | Are you a contractor? Click
                 a(@click="accountType = 'contractor'") &nbsp;here&nbsp;
