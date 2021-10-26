1bc5063c91d59925d39f796f93297c76038d9df2
Alex Wohlbruck <alexwohlbruck@gmail.com>
2021-10-26 15:19:30 -0400

    Manually install vue-socket.io plugin when context changes

--- src/util/socket-io.ts
+++ src/util/socket-io.ts
@@ -1,25 +1,20 @@
 import Vue from 'vue'
-import VueSocketIOExt from 'vue-socket.io-extended'
-import store from '@/store'
 import { io } from 'socket.io-client'
 import { environment } from '@/services/app'
-
-// Initialize a new websocket connection
-function createSocket(url: string) {
-  console.log('creating socket at', url)
-  const socket =  io(url, {
-    path: '/socket.io'
-  })
-
-  return socket
-}
+import store from '@/store'
+import VueSocketIOExt from 'vue-socket.io-extended'
 
 export let socket: any
 
-// Configure vue socketio to use the socket object
-function configureVueSocketio() {
-  console.log('configuring vue socketio', socket)
-  Vue.use(VueSocketIOExt, socket, {
+// Initialize a new websocket connection
+export function createSocket(url = process.env.VUE_APP_API_BASE_URL) {
+  return socket = io(url, {
+    path: '/socket.io'
+  })
+}
+
+function configVueSocketIO(socket: any) {
+  (VueSocketIOExt as any).install(Vue, socket, {
     store,
     actionPrefix: '',
     mutationPrefix: '',
@@ -29,7 +24,6 @@ function configureVueSocketio() {
 
 // When the sandbox environment changes, create a new socket connection
 environment.on('baseUrlChanged', (baseUrl: any) => {
-  console.log('base url changed: ', baseUrl)
   socket = createSocket(baseUrl)
-  configureVueSocketio()
+  configVueSocketIO(socket)
 })
