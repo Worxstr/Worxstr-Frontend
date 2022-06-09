41046ba3d3cfc23dbcf3acde586a51d0cc0d5584
Alex Wohlbruck <alexwohlbruck@gmail.com>
2022-06-09 01:44:47 -0400

    Fix svg images from strapi

--- src/views/landing/CMSSemanticPage.vue
+++ src/views/landing/CMSSemanticPage.vue
@@ -114,6 +114,7 @@ export default class CMSSemanticPage extends Vue {
 
   imageUrl(item: any, attributeName='image', size = 'medium') {
     const attrs = item[attributeName]?.data?.attributes
+    if (!attrs?.formats) return attrs.url
     return attrs?.formats[size]?.url || attrs?.url
   }
 
